425dcd9415bc9839a9db90e6dbdf59ac35c0f966
Aashay30 <tamrakaraashay30@gmail.com>
2024-01-27 12:14:25 +0530

    queries completed

--- SQLQuery.sql
+++ SQLQuery.sql
@@ -1,5 +1,8 @@
 /* KPI */
 
+-- To see the data
+SELECT * FROM pizza_sales
+
 -- Total Revenue
 SELECT SUM(total_price) AS Total_Revenue FROM pizza_sales;
 
@@ -13,13 +16,17 @@ SELECT SUM(quantity) AS Total_pizza_sold FROM pizza_sales
 -- Total Orders
 SELECT COUNT(DISTINCT order_id) AS Total_Orders FROM pizza_sales
 
--- Average Pizzas Per Order
+-- Average Pizzas Per Order ; using 2 decimal points from the generated 10 decimal points
 SELECT CAST(CAST(SUM(quantity) AS DECIMAL(10,2)) / 
-CAST(COUNT(DISTINCT order_id) AS DECIMAL(10,2)) AS DECIMAL(10,2))
+CAST(COUNT(DISTINCT order_id) AS DECIMAL(10,2) ) AS DECIMAL(10,2) )
 AS Avg_Pizzas_per_order
 FROM pizza_sales
 
 /* Daily Trend for Total Orders */
+
+-- DW means day of the week
+
+-- whenever we bring an aggregate function in select we always need to group by the categorical feild
 
 SELECT DATENAME(DW, order_date) AS order_day, COUNT(DISTINCT order_id) AS total_orders 
 FROM pizza_sales
@@ -36,6 +43,7 @@ GROUP BY DATENAME(MONTH, order_date)
 SELECT pizza_category, CAST(SUM(total_price) AS DECIMAL(10,2)) as total_revenue,
 CAST(SUM(total_price) * 100 / (SELECT SUM(total_price) from pizza_sales) AS DECIMAL(10,2)) AS PCT
 FROM pizza_sales
+-- WHERE MONTH(order_date) = 1 -- To see data only for first month i.e. January
 GROUP BY pizza_category
 
 /* Percentage of Sales by Pizza Size */
@@ -43,6 +51,8 @@ GROUP BY pizza_category
 SELECT pizza_size, CAST(SUM(total_price) AS DECIMAL(10,2)) as total_revenue,
 CAST(SUM(total_price) * 100 / (SELECT SUM(total_price) from pizza_sales) AS DECIMAL(10,2)) AS PCT
 FROM pizza_sales
+-- WHERE MONTH(order_date) = 1 -- To see data only for first month i.e. January
+-- WHERE DATEPART(quarter, order_date) = 1 -- To see data for first quarter
 GROUP BY pizza_size
 ORDER BY pizza_size
 
@@ -69,18 +79,21 @@ GROUP BY pizza_name
 ORDER BY Total_Revenue ASC
 
 /* Top 5 Pizzas by Quantity */
+
 SELECT Top 5 pizza_name, SUM(quantity) AS Total_Pizza_Sold
 FROM pizza_sales
 GROUP BY pizza_name
 ORDER BY Total_Pizza_Sold DESC
 
 /* Bottom 5 Pizzas by Quantity */
+
 SELECT TOP 5 pizza_name, SUM(quantity) AS Total_Pizza_Sold
 FROM pizza_sales
 GROUP BY pizza_name
 ORDER BY Total_Pizza_Sold ASC
 
 /* Top 5 Pizzas by Total Orders */
+
 SELECT Top 5 pizza_name, COUNT(DISTINCT order_id) AS Total_Orders
 FROM pizza_sales
 GROUP BY pizza_name
